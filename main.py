92ba61bc557ecacd4a270191afd3231d11b0c56b
Mrwrob <mrwrob@gmail.com>
2023-06-27 21:01:39 +0200

    Throws exception when payload contains invalid JSON data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import uuid
 from typing import Union
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 
@@ -22,14 +22,17 @@ class Item(BaseModel):
     content: Union[str, None] = None
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 @app.put("/LabelingEmotionsDatabase/{item_id}", response_model=str, status_code=201)
 async def update_item(item_id: str, item: Request):
     labels = await item.json()
+    try:
+        json_object = json.loads(labels)
+    except (ValueError, TypeError):
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid JSON payload format"
+        )
+
     with open(f"{uuid.uuid4()}.json", "w") as file_write:
-        json.dump(labels, file_write)
+        json.dump(json_object, file_write)
     return item_id
